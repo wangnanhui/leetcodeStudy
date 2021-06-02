73794eb8c385fe7d16478b3b69f5d3f59149d71e
nanhui.wang <1265530340@qq.com>
2021-06-02 23:39:51 +0800

    正则表达式

--- src/main/java/com/leetcode/DP/LeetCodeDp.java
+++ src/main/java/com/leetcode/DP/LeetCodeDp.java
@@ -759,9 +759,33 @@ public class LeetCodeDp {
         if(p.charAt(m) =='*'){
             return true ;
         }
+
         return s.charAt(m-1) == p.charAt(n-1);
+    }
 
-
+    /**
+     * leetcode 121
+     * 股票问题
+     * 给定一个数组 prices ，它的第 i 个元素 prices[i] 表示一支给定股票第 i 天的价格。
+     *
+     * 你只能选择 某一天 买入这只股票，并选择在 未来的某一个不同的日子 卖出该股票。设计一个算法来计算你所能获取的最大利润。
+     *
+     * 返回你可以从这笔交易中获取的最大利润。如果你不能获取任何利润，返回 0 。
+     *
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     * @param prices
+     * @return
+     */
+    public static int maxProfit(int[] prices) {
+        int max = 0;
+        int buy = Integer.MIN_VALUE;
+        for(int i = 0 ; i < prices.length ; i++){
+            buy = Math.max(buy,-prices[i]); //找最低买入
+            max = Math.max(max,buy + prices[i]);
+        }
+        return max ;
     }
 
 }
