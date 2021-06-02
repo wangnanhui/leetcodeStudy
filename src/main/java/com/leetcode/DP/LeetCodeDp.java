060a4ad6109ea64b7c061789f6f6f1334c9ee342
nanhui.wang <1265530340@qq.com>
2021-06-02 23:16:38 +0800

    正则表达式

--- src/main/java/com/leetcode/DP/LeetCodeDp.java
+++ src/main/java/com/leetcode/DP/LeetCodeDp.java
@@ -666,6 +666,104 @@ public class LeetCodeDp {
         }
         return ret;
     }
+
+    /** leetcode 10
+     * 别人的思路就是清晰 按照别人的写就写出来了
+     * 以一个例子详解动态规划转移方程：
+     * S = abbbbc
+     * P = ab*d*c
+     * 1. 当 i, j 指向的字符均为字母（或 '.' 可以看成一个特殊的字母）时，
+     *    只需判断对应位置的字符即可，
+     *    若相等，只需判断 i,j 之前的字符串是否匹配即可，转化为子问题 f[i-1][j-1].
+     *    若不等，则当前的 i,j 肯定不能匹配，为 false.
+     *
+     *        f[i-1][j-1]   i
+     *             |        |
+     *    S [a  b  b  b  b][c]
+     *
+     *    P [a  b  *  d  *][c]
+     *                      |
+     *                      j
+     *
+     *
+     * 2. 如果当前 j 指向的字符为 '*'，则不妨把类似 'a*', 'b*' 等的当成整体看待。
+     *    看下面的例子
+     *
+     *             i
+     *             |
+     *    S  a  b [b] b  b  c
+     *
+     *    P  a [b  *] d  *  c
+     *             |
+     *             j
+     *
+     *    注意到当 'b*' 匹配完 'b' 之后，它仍然可以继续发挥作用。
+     *    因此可以只把 i 前移一位，而不丢弃 'b*', 转化为子问题 f[i-1][j]:
+     *
+     *          i
+     *          | <--
+     *    S  a [b] b  b  b  c
+     *
+     *    P  a [b  *] d  *  c
+     *             |
+     *             j
+     *
+     *    另外，也可以选择让 'b*' 不再进行匹配，把 'b*' 丢弃。
+     *    转化为子问题 f[i][j-2]:
+     *
+     *             i
+     *             |
+     *    S  a  b [b] b  b  c
+     *
+     *    P [a] b  *  d  *  c
+     *       |
+     *       j <--
+     *
+     * 3. 冗余的状态转移不会影响答案，
+     *    因为当 j 指向 'b*' 中的 'b' 时, 这个状态对于答案是没有用的,
+     *    原因参见评论区 稳中求胜 的解释, 当 j 指向 '*' 时,
+     *    dp[i][j]只与dp[i][j-2]有关, 跳过了 dp[i][j-1].
+     * @param s
+     * @param p
+     * @return
+     */
+    public static boolean isMatch(String s , String p ){
+        int m = s.length() ;
+        int n = p.length() ;
+        boolean [][] dp = new boolean[m+1][n+1];
+        dp[0][0] = true;
+        for (int i = 0; i <= m; i++) {
+            for (int j = 1; j <= n ; j++) {
+                char c = p.charAt(j-1);
+                if(c == '*'){
+                    dp[i][j] = dp[i][j-2];
+                    if(match(s,p,i,j-1)){//如果匹配上了
+                        dp[i][j] = dp[i][j] || dp[i-1][j];
+                    }
+
+
+                }else{
+                    if(match(s,p,i,j)){
+                        dp[i][j] =dp[i-1][j-1];
+                    }
+                }
+
+            }
+        }
+        return dp[m][n];
+    }
+
+    private static boolean match(String s, String p , int m , int n){
+        if(m == 0 )
+            return false;
+        if(p.charAt(m) =='*'){
+            return true ;
+        }
+        return s.charAt(m-1) == p.charAt(n-1);
+
+
+    }
+
 }
 
 
