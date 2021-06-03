396862fa1876abc104267f224bb3047294f0390b
nanhui.wang <1265530340@qq.com>
2021-06-03 22:58:31 +0800

    打家劫舍

--- src/main/java/com/leetcode/DP/LeetCodeDp.java
+++ src/main/java/com/leetcode/DP/LeetCodeDp.java
@@ -788,6 +788,53 @@ public class LeetCodeDp {
         return max ;
     }
 
+    /**
+     * 你是一个专业的小偷，计划偷窃沿街的房屋，每间房内都藏有一定的现金。这个地方所有的房屋都 围成一圈 ，这意味着第一个房屋和最后一个房屋是紧挨着的。同时，相邻的房屋装有相互连通的防盗系统，如果两间相邻的房屋在同一晚上被小偷闯入，系统会自动报警 。
+     *
+     * 给定一个代表每个房屋存放金额的非负整数数组，计算你 在不触动警报装置的情况下 ，今晚能够偷窃到的最高金额。
+     *
+     *
+     * 输入：nums = [2,3,2]
+     * 输出：3
+     * 解释：你不能先偷窃 1 号房屋（金额 = 2），然后偷窃 3 号房屋（金额 = 2）, 因为他们是相邻的。
+     *
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/house-robber-ii
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     *
+     *
+     *
+     *
+     * 思路 ： 收尾相连 ， 那么偷第一间就不能偷最后一间 ，同理 偷最后一件就不能偷第一间
+     *  所以分两种情况 第一次偷第一间 最后一间不偷
+     *              偷最后一件，第一间不偷
+     * @param nums
+     * @return
+     */
+    public static int rob(int[] nums) {
+        if(nums.length == 0)
+            return 0;
+        if(nums.length == 1)
+            return nums[0];
+
+        int n = nums.length;
+        int [] dp = new int[n + 1];
+        dp[1] = nums[0];
+        for(int i = 2;  i < n ; i ++){
+            dp[i] = Math.max(dp[i-2] + nums[i-1],dp[i-1]);
+        }
+
+        int max = dp[n-1] ;//偷第一间的话最大值
+        dp = new int[n+1];
+        for(int i = 2;  i <= n ; i ++){
+            dp[i] = Math.max(dp[i-2] + nums[i-1],dp[i-1]);
+        }
+        //dp[n] 偷最后一间的值
+        return Math.max(max,dp[n]);
+
+    }
+
+
 }
 
 
