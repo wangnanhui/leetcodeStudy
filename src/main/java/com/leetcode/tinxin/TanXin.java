fe36e7f4b07b1242c8ed9e0c5b722248b499b5ef
wangnanhui <wangnanhui@tal.com>
2021-07-05 18:01:35 +0800

    20210705 发糖果

--- src/main/java/com/leetcode/tinxin/TanXin.java
+++ src/main/java/com/leetcode/tinxin/TanXin.java
@@ -2,7 +2,8 @@ package com.leetcode.tinxin;
 
 public class TanXin {
     public static void main(String[] args) {
-
+        TanXin t = new TanXin();
+        t.candy(new int[]{1,3,2,2,1});
     }
 
     /**
@@ -34,4 +35,41 @@ public class TanXin {
         }
         return c ;
     }
+
+    /**
+     * leetcode 135. 分发糖果
+     * @param ratings
+     * @return
+     */
+    public int candy(int [] ratings) {
+        int candy = 0 ;
+        int [] candys = new int[ratings.length];
+        candys[0] = 1 ;
+        for (int i = 1; i <ratings.length ; i++) {
+            candys[i] = 1;
+            if(ratings[i] > ratings[i-1]){
+                candys[i] = Math.max(candys[i] , candys[i-1]) + 1 ;
+            }
+        }
+        for (int i = ratings.length - 1 ; i >0 ; i--) {
+            if(ratings[i] < ratings[i - 1 ]){
+                candys[i-1] = Math.max(candys[i]+1 , candys[i-1]) ;
+            }
+        }
+        for (int i = 0; i < candys.length; i++) {
+            candy+=candys[i];
+        }
+        System.out.println(candy);
+        return candy;
+    }
+
+
+
+
+
+
+
+
+
+
 }
